6c7a15cd36d86bc65044209262b9e9bc7cb376ef
Ali Goldani <ali.goldani@ymail.com>
2019-12-15 01:22:30 +0330

    first try

---
+++ neural_net.py
@@ -0,0 +1,67 @@
+#%%
+import keras
+import numpy as np
+import mido
+from keras import Sequential
+from keras.layers import Dense
+from mido import MidiFile
+import pandas as pd
+import os
+import csv
+import glob as glob
+
+# %% Create train dataset
+with open('data/query-7.csv', mode='w') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    for file in glob.glob('validation/query/*'):
+        mid = MidiFile(file)
+        vector = []
+        for track in mid.tracks:
+            for msg in track:
+                if hasattr(msg, 'note'):
+                    vector.append(msg.note)
+
+        for i in range(len(vector) - 6):
+            csv_writer.writerow(vector[i: i + 7])
+
+# %% Load data
+data = np.loadtxt('data/query-7.csv', delimiter=',')
+X = data[:, 3]  # label
+
+# %% Create Model
+model = Sequential()
+model.add(Dense(7, input_dim=7, activation='relu'))
+model.add(Dense(14, activation='relu'))
+model.add(Dense(28, activation='relu'))
+model.add(Dense(56, activation='relu'))
+model.add(Dense(112, activation='relu'))
+model.add(Dense(1, activation='relu'))
+
+model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+
+# %% Fit model
+model.fit(data, X, epochs=100, batch_size=64)
+
+# %%
+model.save('models/NN_first.h5')
+
+# %% Evaluation
+_, accuracy = model.evaluate(data, X)
+print('Accuracy: %.2f' % (accuracy*100))
+
+# %% Predicting
+predictions = model.predict(data)
+#
+rounded = [round(x[0]) for x in predictions]
+
+for i, predict in enumerate(rounded):
+    if predict != X[i]:
+        print('predict: %d, value: %d' % (predict, X[i]))
+# print(rounded)
+#
+# predictions = model.predict_classes(data)
+#
+# for i in range(5):
+#     print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], X[i]))
+
+
